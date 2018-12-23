243d79cfd05d209695c3a6ddb210be82f4c3ad55
wdbbdpd <m13301295501_1@163.com>
2018-12-23 18:43:38 +0800

    showEdit
    双击每一个 todo 的时候显示编辑文本框

--- src/components/allList/index.js
+++ src/components/allList/index.js
@@ -1,12 +1,20 @@
 import { mapMutations } from 'vuex'
 export default {
   name: 'all',
+  data () {
+    return {
+      tmpInfo: {}
+    }
+  },
   computed: {
     todoList () {
       return this.$store.state.todoList
     }
   },
   methods: {
-    ...mapMutations(['delTodo', 'changeState'])
+    ...mapMutations(['delTodo', 'changeState']),
+    showEdit (item) {
+      this.tmpInfo = JSON.parse(JSON.stringify(item))
+    }
   }
 }
