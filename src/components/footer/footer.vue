f0e739231e5f7450701d8253c25f195b5a53fff8
wdbbdpd <m13301295501_1@163.com>
2018-12-23 16:55:10 +0800

    拆分组件
    拆分出 section 和 footer 两个主要组件

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="info">
+    <p>Double-click to edit a todo</p>
+    <p>Template by <a href="javascript:;">Sindre Sorhus</a></p>
+    <p>Created by <a href="javascript:;">you</a></p>
+    <p>Part of <a href="javascript:;">TodoMVC</a></p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer-html'
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/section/section.vue
@@ -0,0 +1,3 @@
+<template src="./index.html"></template >
+<script src="./index.js"></script >
+<style src="./index.css" scoped></style >
